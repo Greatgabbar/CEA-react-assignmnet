01653be7d15f41a6e49be536c22958e7827da232
yashb007 <bansaly37@gmail.com>
2021-03-21 19:13:59 +0530

    next

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const Routing = () => {
  
   return (
     <Switch>
-    <Route exact path='/home'> <Home /> </Route>
+    <Route exact path='/'> <Home /> </Route>
     <Route  path='/user'> <User /> </Route>
     <Route  path='/create'> <CreatePost /> </Route>
     <Route  path='/update'> <Update /> </Route>
